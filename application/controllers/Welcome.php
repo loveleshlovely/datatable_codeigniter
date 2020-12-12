13dfc8bc765b294601874a35a1ba198183c18c60
lovelesh <lovelesh@xcrino.com>
2020-12-12 20:38:15 +0530

    final sumission

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,20 +6,57 @@ class Welcome extends CI_Controller {
 	/**
 	 * Index Page for this controller.
 	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
+	/**
+	 * @method use to get data from get data method of welcome model
+	 */
+	public function getData() {
+		if(isset($_GET['search']['value'])){
+			$search = $_GET['search']['value'];
+		}
+		else{
+			$search = '';
+		}
+		if(isset($_GET['length'])){
+			$limit = $_GET['length'];
+		}
+		else{
+			$limit = 10;
+		}
+		if(isset($_GET['start'])){
+			$ofset = $_GET['start'];
+		}
+		else{
+			$ofset = 0;
+		}
+		$orderType = $_GET['order'][0]['dir'];
+		$nameOrder = $_GET['columns'][$_GET['order'][0]['column']]['name'];
+
+		$records = $this->Welcome_model->getData($limit , $search ,  $ofset , $nameOrder , $orderType);
+		$data = array();
+		$i=0+$ofset;
+		foreach ($records['data']  as $row)
+		{
+			if($row['status'] == 0){ $status =   ' <span class="btn btn-danger btn-sm tgl_change_post danger" data-status="1" id="post_'.$row['user_id'].'" data-id="'.$row['user_id'].'">Inactive</span>';}
+			elseif($row['status'] == 1) { $status =  '<span class="btn btn-success btn-sm tgl_change_post" data-status="0" data-id="'.$row['user_id'].'" id="post_'.$row['user_id'].'">Active</span>';}
+
+//            update_post_status('ci_ads' , $row['id']);
+			$data[]= array(
+				++$i,
+				$row['username'],
+				$row['first_name']. ' '. $row['last_name'],
+				$row['gender'],
+				$status,
+			);
+		}
+		$records['data']=$data;
+		echo json_encode($records);
+
+	}
+
+
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>jQuery Datatable</title>
 
 	<style type="text/css">
 
@@ -64,26 +64,54 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		box-shadow: 0 0 8px #D0D0D0;
 	}
 	</style>
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
 </head>
 <body>
-
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<table id="example" class="display" style="width:100%">
+		<thead>
+		<tr>
+			<th>Id</th>
+			<th>User Name</th>
+			<th>Name</th>
+			<th>Gender</th>
+			<th>Status</th>
+		</tr>
+		</thead>
+	</table>
 </div>
 
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script>
+	$(document).ready(function() {
+		tableLoad();
+		function tableLoad() {
+			var table = $('#example').DataTable(
+					{
+						"processing": true,
+						"serverSide": true,
+						"ajax": "<?=base_url('index.php/Welcome/getData')?>",
+						"order": [[1, 'desc']],
+						"columnDefs": [
+							{"targets": 0, "name": "id", 'searchable': true, 'orderable': true},
+							{"targets": 1, "name": "username", 'searchable': true, 'orderable': true},
+							{"targets": 2, "name": "first_name", 'searchable': true, 'orderable': true},
+							{"targets": 3, "name": "gender", 'searchable': true, 'orderable': true},
+							{"targets": 4, "name": "status", 'searchable': true, 'orderable': true},
+
+						]
+					});
+		}
+	} );
+</script>
 </body>
+
 </html>
